314841be9388ea5603803d4d525017f083999819
ezzoubeirj <ezzoubeir.jabrane@gmail.com>
2025-08-31 17:31:06 +0100

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,12 +13,17 @@ export default function RootLayout({ children }) {
             !function(f,b,e,v,n,t,s){
               if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              if(!f._fbq)f._fbq=n;
+              n.push=n;n.loaded=!0;n.version='2.0';
               n.queue=[];t=b.createElement(e);t.async=!0;
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)
             }(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
+
+            // Expose fbq globally
+            window.fbq = window.fbq || fbq;
+
             fbq('init', '371485473841753');
           `}
         </Script>
