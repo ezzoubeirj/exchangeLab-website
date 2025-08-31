37913ab74bfd0777c1903382f1bdde255f8b181d
ezzoubeirj <ezzoubeir.jabrane@gmail.com>
2025-08-31 17:24:03 +0100

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
-import './globals.css';
-import { Analytics } from '@vercel/analytics/react';
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 import FBPixelListener from "./fbpixel-listener";
 
@@ -7,29 +7,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Meta Pixel base code */}
-        <Script id="fb-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)
-          `
-        }} />
-        <Script id="fb-pixel-init" strategy="afterInteractive" dangerouslySetInnerHTML={{
-          __html: `
+        {/* Meta Pixel Base Code */}
+        <Script id="fb-pixel-base" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '371485473841753');
-            fbq('track', 'PageView');
-          `
-        }} />
+          `}
+        </Script>
         <noscript>
-          <img height="1" width="1" style={{display:"none"}}
-            src="https://www.facebook.com/tr?id=371485473841753&ev=PageView&noscript=1" />
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=371485473841753&ev=PageView&noscript=1"
+          />
         </noscript>
       </head>
       <body>
-        <FBPixelListener />
         {children}
+        <FBPixelListener />
         <Analytics />
       </body>
     </html>
