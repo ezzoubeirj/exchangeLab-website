77f9f878707ec8e76a0cc3b2a6bbeb766a462947
ezzoubeirj <ezzoubeir.jabrane@gmail.com>
2025-08-31 16:37:32 +0100

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,35 @@
 import './globals.css';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      {children}
-      <Analytics />
-    </>
+    <html lang="en">
+      <head>
+        {/* Meta Pixel base code */}
+        <Script id="fb-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)
+          `
+        }} />
+        <Script id="fb-pixel-init" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            fbq('init', '371485473841753');
+            fbq('track', 'PageView');
+          `
+        }} />
+        <noscript>
+          <img height="1" width="1" style={{display:"none"}}
+            src="https://www.facebook.com/tr?id=371485473841753&ev=PageView&noscript=1" />
+        </noscript>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
   );
 }
